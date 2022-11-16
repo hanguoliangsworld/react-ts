94e4b6eafa7c4d46af2d0e99530796dbdb5b9b41
hgl <1432395474@qq.com>
2022-11-16 21:52:18 +0800

    fix: 添加eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,4 @@
-module.exports = {
+/* module.exports = {
   extends: ['tuya-panel'],
   rules: {
     camelcase: 0,
@@ -18,6 +18,24 @@ module.exports = {
       {
         object: 'disallowedObjectName',
         property: 'disallowedPropertyName',
+      },
+    ],
+  },
+};
+ */
+module.exports = {
+  extends: ["react-app", "plugin:prettier/recommended"],
+  rules: {
+    // allow debugger during development
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-console": 0,
+    "prettier/prettier": [
+      "error",
+      {
+        singleQuote: false, //使用单引号
+        trailingComma: "all", //有效的尾随逗号（对象、数组等）
+        bracketSameLine: true,
+        endOfLine: "auto",
       },
     ],
   },
---
+++ .prettierrc.js
@@ -0,0 +1,5 @@
+module.exports = {
+  singleQuote: false, //使用单引号
+  trailingComma: "all", //有效的尾随逗号（对象、数组等）
+  bracketSameLine: true,
+};
